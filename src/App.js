a376b7a09102d6b7e9f35aed9374459baa797139
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-07-29 22:15:16 +0900

    setting

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import styled from "styled-components";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ItemDetail from "./pages/ItemDetail";
+import Footer from "./components/Footer";
+import Basket from "./pages/Basket";
 
 function App() {
   return (
-    <div>
-    </div>
+    <AppBody>
+      <Navbar />
+      <MainBody>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/detail" element={<ItemDetail />} />
+          <Route path="/basket" element={<Basket />} />
+        </Routes>
+      </MainBody>
+      <Footer />
+    </AppBody>
   );
 }
+
+const AppBody = styled.div`
+  margin: 0;
+  padding: 0;
+`;
+
+const MainBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 100px;
+  width: 100%;
+  height: auto;
+`;
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <MainFooter>
+      <MainLeftFrame>Left11</MainLeftFrame>
+      <MainRightFrame>Right11</MainRightFrame>
+    </MainFooter>
+  );
+};
 
-export default Footer
+const MainFooter = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+const MainLeftFrame = styled.div`
+  margin-left: 1rem;
+`;
+
+const MainRightFrame = styled.div`
+  margin-right: 1rem;
+`;
+
+const FooterSection = styled.div`
+  width: 100%;
+  border-width: 0;
+  border-style: solid;
+`;
+
+export default Footer;
